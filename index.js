73fb4e802942a1e0615e6c195c02d5cfd402d065
Thomas <t.w.youens2@newcastle.ac.uk>
2023-06-12 21:47:43 +0100

    add db connection and implemented env vara

--- index.js
+++ index.js
@@ -1,15 +1,23 @@
 const app  = require("express")();
+require('dotenv').config()
 const port = 8080;
+const { default: mongoose } = require("mongoose");
 const assets = require('./src/routes/assets')
 
+async function connect() {
+    try{
+        await mongoose.connect(process.env.DB_URI)
+        console.log("Database connected");
+    } catch (error) {
+        console.error("Error: An error occured when trying to connect to MongoDB database.")
+        console.error(error)
+    }
+}
+
 app.get("/", (req, res) => {
-    res.status(200).send(
-        {
-            key: "Hello"
-        }
-    )
+    res.status(404).send("Invalid URL")
 });
 
-app.use('/assets',assets)                                                                                                                               
-
-app.listen(port, ()=>{console.log(`Server started! vist: http://localhost:${port}`)});
+app.use('/assets',assets)
+connect()
+app.listen(process.env.APP_PORT, ()=>{console.log(`Server started! vist: http://localhost:${port}`)});
