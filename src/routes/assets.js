2a7e7c8c7443ee71079fd8ad65fb1b7006594f7b
Thomas <t.w.youens2@newcastle.ac.uk>
2023-06-16 23:24:50 +0100

    moved methods to controller

--- src/routes/assets.js
+++ src/routes/assets.js
@@ -1,47 +1,19 @@
 const express = require('express')
 const Asset = require('../models/assetsModel')
 const router = express.Router()
+const assetsController = require('../controllers/assetsController')
 
 router.use((req, res, next) => {
     next()
 })
 
-router.get('/', (req, res) => {
-    res.status(200).send(
-        [
-            {
-                id: 1,
-                name: "My PC 1",
-                type: "computer"
-            },
-            {
-                id: 2,
-                name: "My Laptop 1",
-                type: "laptop"
-            }  
-        ]
-    )
-})
-router.post('/', async (req, res) => {
-    try{
-        newAsset = await Asset.create(req.body)
-        res.status(201).send(newAsset);
-    } catch(error){
-        console.error(error)
-        res.status(500).send({error:error.message})
-    }
-})
-router.get('/asset/:id', (req, res) => {
-    const { id } = req.params;
-    res.status(200).send(
-        [
-            {
-                id: id,
-                name: "My Computer "+id,
-                type: "computer"
-            },
-        ]
-    )
-})
+// GET all assets
+router.get('/', assetsController.get)
+
+// GET asset by ID
+router.get('/asset/:id', assetsController.getbyID)
+
+// POST new asset
+router.post('/', assetsController.create)
 
 module.exports = router
